e7ff879b5264d34b4b70a6cc8df9336ed9ddb899
Juby210 <31005896+Juby210@users.noreply.github.com>
2021-08-22 01:40:06 +0200

    Add aliucord-SNAPSHOT helper to discord configuration (#1)

--- src/main/kotlin/com/aliucord/gradle/configuration/DiscordConfigurationProvider.kt
+++ src/main/kotlin/com/aliucord/gradle/configuration/DiscordConfigurationProvider.kt
@@ -21,6 +21,7 @@ import com.aliucord.gradle.getAliucord
 import com.googlecode.d2j.dex.Dex2jar
 import com.googlecode.d2j.reader.BaseDexFileReader
 import com.googlecode.d2j.reader.MultiDexFileReader
+import groovy.json.JsonSlurper
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Dependency
 import java.lang.Integer.parseInt
@@ -28,12 +29,25 @@ import java.net.URL
 import java.nio.file.Files
 
 class DiscordConfigurationProvider : IConfigurationProvider {
+    companion object {
+        private var aliucordSnapshot: Int? = null
+    }
+
     override val name: String
         get() = "discord"
 
     override fun provide(project: Project, dependency: Dependency) {
+        val version = if (dependency.version == "aliucord-SNAPSHOT") run fetch@ {
+            if (aliucordSnapshot != null) return@fetch aliucordSnapshot!!
+            project.logger.lifecycle("Fetching discord version")
+            val data = JsonSlurper().parse(URL("https://raw.githubusercontent.com/Aliucord/Aliucord/builds/data.json")) as Map<*, *>
+            val versionCode = parseInt(data["versionCode"] as String)
+            project.logger.lifecycle("Fetched discord version: $versionCode")
+            versionCode.also { aliucordSnapshot = versionCode }
+        } else parseInt(dependency.version)
+
         val extension = project.extensions.getAliucord()
-        val discord = DiscordInfo(extension, parseInt(dependency.version))
+        val discord = DiscordInfo(extension, version)
         extension.discord = discord
 
         discord.cache.mkdirs()
